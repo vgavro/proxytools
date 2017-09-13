ec3dd94028c8286046ad9a169652e1aa715923bf
Victor Gavro <vgavro@gmail.com>
2017-09-13 11:38:53 +0300

    freeproxylistnet - added us and uk urls; socks proxy moved to same fetcher

--- proxytools/fetchers/freeproxylistnet.py
+++ proxytools/fetchers/freeproxylistnet.py
@@ -7,7 +7,10 @@ from ..proxyfetcher import ConcreteProxyFetcher, Proxy
 
 
 class FreeProxyListNet(ConcreteProxyFetcher):
-    URL = 'https://free-proxy-list.net'
+    HTTP_URL = 'https://free-proxy-list.net'
+    SOCKS_URL = 'https://www.socks-proxy.net'
+    US_URL = 'https://www.us-proxy.org'
+    GB_URL = 'https://free-proxy-list.net/uk-proxy.html'
 
     ANONYMITY_MAP = {
         'elite proxy': Proxy.ANONYMITY.HIGH,
@@ -15,14 +18,21 @@ class FreeProxyListNet(ConcreteProxyFetcher):
         'transparent': Proxy.ANONYMITY.TRANSPARENT,
     }
 
-    TYPES_MAP = {
+    HTTPS_TYPES_MAP = {
         'yes': (Proxy.TYPE.HTTP, Proxy.TYPE.HTTPS),
         'no': (Proxy.TYPE.HTTP,)
+    }
+
+    SOCKS_TYPES_MAP = {
+        'Socks4': (Proxy.TYPE.SOCKS4,),
+        'Socks5': (Proxy.TYPE.SOCKS5,),
     }
 
     TIME_REGEXPS = (
         re.compile('()()(\d+) seconds? ago'),
         re.compile('()(\d+)() minutes? ago'),
+        re.compile('(\d+) hours?()() ago'),
+        re.compile('(\d+) hours? (\d+)() minutes? ago'),
     )
 
     def _parse_time(self, value):
@@ -36,19 +46,40 @@ class FreeProxyListNet(ConcreteProxyFetcher):
     def _parse_country(self, value):
         return value if value != 'Unknown' else None
 
-    def _parse_proxy_row(self, tr):
+    def _parse_http_proxy_row(self, tr):
         return Proxy(
             tr[0].text + ':' + tr[1].text,
-            types=self.TYPES_MAP[tr[6].text],  # "Https" field
+            types=self.HTTPS_TYPES_MAP[tr[6].text],  # "Https" field
             country=self._parse_country(tr[2].text),
             anonymity=self.ANONYMITY_MAP[tr[4].text],
             success_at=self._parse_time(tr[7].text),
         )
 
+    def _parse_socks_proxy_row(self, tr):
+        return Proxy(
+            tr[0].text + ':' + tr[1].text,
+            types=self.SOCKS_TYPES_MAP[tr[4].text],
+            country=self._parse_country(tr[2].text),
+            success_at=self._parse_time(tr[7].text),
+        )
+
     def worker(self):
-        resp = self.session.get(self.URL)
+        if not self.countries or 'GB' in self.countries:
+            self.spawn(self.page_worker, self.GB_URL, 'http')
+        if not self.countries or 'US' in self.countries:
+            self.spawn(self.page_worker, self.US_URL, 'http')
+        if (not self.types or
+           self.types.intersection([Proxy.TYPE.SOCKS4, Proxy.TYPE.SOCKS5])):
+            self.spawn(self.page_worker, self.SOCKS_URL, 'socks')
+        return self.page_worker(self.HTTP_URL, 'http')
+
+    def page_worker(self, url, proxy_type):
+        resp = self.session.get(url)
         resp.raise_for_status()
         doc = html.fromstring(resp.text)
 
         for tr in doc.cssselect('table#proxylisttable tbody')[0]:
-            yield self._parse_proxy_row(tr)
+            if proxy_type == 'http':
+                yield self._parse_http_proxy_row(tr)
+            else:
+                yield self._parse_socks_proxy_row(tr)
