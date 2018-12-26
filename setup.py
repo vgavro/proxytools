c7acafe99a934775725de2f97ed34edb89e4af93
Victor Gavro <vgavro@gmail.com>
2018-12-27 00:27:43 +0700

    version set to 0.0.3

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 
 setup(
     name='proxytools',
-    version='0.0.2',
+    version='0.0.3',
     description='http://github.com/vgavro/proxytools',
     long_description='http://github.com/vgavro/proxytools',
     license='BSD',
